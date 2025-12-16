06721431fe03a7af8a1da75be0ff476e8598f088
aymen99tn <aymenbasly@gmail.com>
2025-12-16 16:46:21 -0400

    testing two registers reading

--- test_rpi_opta.py
+++ test_rpi_opta.py
@@ -5,6 +5,7 @@ import time
 SERVER_HOST = "0.0.0.0"  # CRITICAL: RPI-2's fixed IP
 SERVER_PORT = 502
 TEST_POWER_INT = 25552        # Test value: 255.55 kW scaled by 100
+TEST_TiME_INT=1234567890   # Test value: Unix time
 
 # 1. Initialize Modbus Server
 #    (No need to pass register sizes; v0.3.0 handles this dynamically)
@@ -15,6 +16,8 @@ server = ModbusServer(host=SERVER_HOST, port=SERVER_PORT, no_block=True)
 try:
     print(f"Setting initial value {TEST_POWER_INT} to register 40001...")
     server.data_bank.set_holding_registers(0, [TEST_POWER_INT])
+    print(f"Setting initial time value {TEST_TiME_INT} to register 40002...")
+    server.data_bank.set_holding_registers(1, [TEST_TiME_INT])
 except Exception as e:
     print(f"Error setting registers: {e}")
 
@@ -26,7 +29,7 @@ try:
     
     while True:
         # Optional: Print the current value to confirm it's stored
-        current_val = server.data_bank.get_holding_registers(0, 1)
+        current_val = server.data_bank.get_holding_registers(0, 1,2)
         if current_val:
             # print(f"Current Register Value: {current_val[0]}")
             pass
